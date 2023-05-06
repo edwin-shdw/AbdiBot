13646e8afca314afd14f213539f040ea3cb0a4bb
edwin-shdw <edwin.krivoseev@gmx.de>
2023-05-06 20:23:57 +0200

    feat: add /info command

---
+++ commands-global/info.js
@@ -0,0 +1,22 @@
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('info')
+    .setDescription('Zeige Infos über den Bot'),
+
+    async execute(interaction) {
+        const embed = new EmbedBuilder()
+        .setColor(0x5865F2)
+        .setTitle('Info')
+        .setDescription(
+            'Ich bin Abdi, ein administrativer Discord Bot für den [IT-ler BSZ III](https://discord.gg/Hp6v3ry7Us) '
+            +'Server. Ich bin ein [Open Source Projekt](https://github.com/edwin-shdw/AbdiBot) und wurde von '
+            +'[Edwin](https://edwin-shdw.de) und meinen [Contributers](https://github.com/edwin-shdw/AbdiBot/graphs/contributors) '
+            +'erstellt. Unterstütz mich doch in dem du an meinem Code arbeitest oder falls coden nicht deins ist mit einer kleinen '
+            +'[Spende](https://paypal.me/plsineedthatcash), um meine Server kosten zu zahlen!'
+        )
+
+        interaction.reply({ embeds: [embed] });
+    }
+}
