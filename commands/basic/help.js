9228b3fb22228e66cafb7c6bda327db6614df862
edwin-shdw <edwin.krivoseev@gmx.de>
2023-05-06 21:53:22 +0200

    feat:  add /help command

---
+++ commands/basic/help.js
@@ -0,0 +1,23 @@
+const { EmbedBuilder, SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { descriptions } = require('../../assets/commands/help.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('help')
+    .setDescription('Zeigt eine Liste mit allen Commands und Erklärungen'),
+
+    async execute(interaction) {
+        const commands = await interaction.guild.commands.fetch();
+
+        const embed = new EmbedBuilder()
+        .setTitle(':information_source: Ich bin hier um zu helfen!')
+        .setDescription('Hier ist eine Liste mit allen Commands und einer erklärung:')
+        .addFields(
+            commands.filter(command => !command.defaultMemberPermissions).map(command => {
+                return { name: `</${command.name}:${command.id}>`, value: ('```' + `${descriptions[command.name]}` + '```') };
+            })
+        )
+
+        interaction.reply({ embeds: [embed], ephemeral: true });
+    }
+}
