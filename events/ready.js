505bf5d19b9741bff0b029418a19f3415dfb5328
edwin-shdw <edwin.krivoseev@gmx.de>
2023-05-23 23:43:13 +0200

    feat: change activity over time

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,27 @@
-const { Events } = require('discord.js');
+const { ActivityType, Events } = require('discord.js');
+const { guildID } = require('../config.json');
+
+let mode = 0;
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
+
+        setInterval(() => {
+            if(mode == 0) {
+                client.guilds.fetch(guildID).then(guild => {
+                    guild.members.fetch().then(members => {
+                        const memberCount = members.filter(member => !member.user.bot).size;
+                        client.user.setActivity(`${memberCount} Members`, { type: ActivityType.Watching });
+                    });
+                });
+                mode++;
+            } else if(mode == 1) {
+                client.user.setActivity("Source Code", { type: ActivityType.Watching });
+                mode--;
+            }
+        }, 90_000);
     }
 }
