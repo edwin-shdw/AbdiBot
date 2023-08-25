0ef7ddbafc61ac74376019a461785f3e7f3afa21
edwin-shdw <edwin.krivoseev@gmx.de>
2023-08-25 20:56:20 +0200

    feat: change activity over time

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,4 +1,5 @@
-import { Events } from 'discord.js';
+import { ActivityType, Events } from 'discord.js';
+import { guildID } from '../configs/config.json';
 import AbdiClient from '../structures/AbdiClient';
 
 module.exports = {
@@ -6,5 +7,22 @@ module.exports = {
     once: true,
     execute(client: AbdiClient) {
         console.log(`Ready! Logged in as ${client.user?.tag}`);
+
+        let mode = 0;
+        setInterval(() => {
+            if(mode == 0) {
+                client.guilds.fetch(guildID).then(guild => {
+                    guild.members.fetch().then(members => {
+                        const memberCount = members.filter(member => !member.user.bot).size;
+                        client.user?.setActivity(`${memberCount} Members`, { type: ActivityType.Watching });
+                    });
+                });
+                mode++;
+            }
+            else if(mode == 1) {
+                client.user?.setActivity('Source Code', { type: ActivityType.Listening });
+                mode--;
+            }
+        }, 90_000)
     }
 }
